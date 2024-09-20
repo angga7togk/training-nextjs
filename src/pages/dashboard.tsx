40f4f8cef993df9886d3c0ac43f5f20df83ea8c2
Angga7Togk <kiplihode123321@gmail.com>
2024-09-20 21:04:16 +0700

    First Commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const dashboard = () => {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <div>dashboard</div>
+    </>
+  );
+};
+
+export default dashboard;
